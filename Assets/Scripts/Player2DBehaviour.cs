300a2e94770010489ff1f275755705208b74e6bf
Meekr0 <janmichalski@hotmail.com>
2021-09-11 01:27:03 +0200

    Player2DBehavior, PlayerPrefabs, Strzelanie

--- Assets/Scripts/Player2DBehaviour.cs
+++ Assets/Scripts/Player2DBehaviour.cs
@@ -6,13 +6,14 @@ public class Player2DBehaviour : ElympicsMonoBehaviour, IUpdatable
 	private readonly int _movingAnimatorBool = Animator.StringToHash("IsWalking");
 
 	[SerializeField] private Animator  characterAnimator  = null;
-	[SerializeField] private float     speed              = 2;
-	[SerializeField] private float     jumpSpeed          = 4;
+	[SerializeField] private float     speed              = 5;
+    [SerializeField] private float     hp                 = 3;
+    [SerializeField] private float     jumpSpeed          = 4;
 	[SerializeField] private float     force              = 100;
 	[SerializeField] private float     fireDuration       = 0.4f;
 	[SerializeField] private string    ballPrefabName     = "BlueBall";
-	[SerializeField] private Transform ballAnchor         = null;
-	[SerializeField] public  int       associatedPlayerId = ElympicsPlayer.INVALID_ID;
+	[SerializeField] private Transform ballAnchor        = null;
+    [SerializeField] public  int       associatedPlayerId = ElympicsPlayer.INVALID_ID;
 
 	// using ElympicsFloats for timer allows you to predict their change, allowing for ball spawn prediction
 	// it's not neccessary, but without this prediction the spawned balls might appear and then disappear and appear again because of lags
@@ -33,7 +34,7 @@ public class Player2DBehaviour : ElympicsMonoBehaviour, IUpdatable
 		var direction = Vector2.right * horizontalAxis;
 		var velocity = direction * speed;
 		_rigidbody.velocity = new Vector2(velocity.x, _rigidbody.velocity.y);
-		ApplyRotation(horizontalAxis);
+		//ApplyRotation(horizontalAxis);
 	}
 
 	private void ApplyRotation(float movementDirection)
@@ -80,13 +81,36 @@ public class Player2DBehaviour : ElympicsMonoBehaviour, IUpdatable
 
 	private void SpawnBall()
 	{
-		// using ElympicsInstantiate to synchronize instantiating a prefab
-		// the instantiated prefab has to be in Resources with this name
-		var newBall = ElympicsInstantiate(ballPrefabName);
-		newBall.transform.position = ballAnchor.transform.position;
-		newBall.transform.rotation = ballAnchor.transform.rotation;
-		newBall.GetComponent<Rigidbody2D>().AddForce((newBall.transform.right + newBall.transform.up) * force);
-	}
+        // using ElympicsInstantiate to synchronize instantiating a prefab
+        // the instantiated prefab has to be in Resources with this name
+
+        var newBall = ElympicsInstantiate(ballPrefabName);
+
+
+        if (this.gameObject.name == "Player1")
+        {
+
+            newBall.transform.position = ballAnchor.transform.position;
+            newBall.transform.rotation = ballAnchor.transform.rotation;
+            newBall.GetComponent<Rigidbody2D>().AddForce((-newBall.transform.right/2 + newBall.transform.up) * force);
+
+            var newBall2 = ElympicsInstantiate(ballPrefabName);
+
+            newBall2.transform.position = ballAnchor.transform.position;
+            newBall2.transform.rotation = ballAnchor.transform.rotation;
+            newBall2.GetComponent<Rigidbody2D>().AddForce((newBall2.transform.right/2 + newBall2.transform.up) * force);
+            
+        }
+        else if (this.gameObject.name == "Player2")
+        {
+
+            newBall.transform.position = ballAnchor.transform.position;
+            newBall.transform.rotation = ballAnchor.transform.rotation;
+            newBall.GetComponent<Rigidbody2D>().AddForce((-newBall.transform.up) * force);
+
+        }
+
+    }
 
 	// this fixed update is called during prediction loop, you can use it to predict changes to ElympicsVars, 
 	// for example, timers or health lost when standing in lava
@@ -104,4 +128,42 @@ public class Player2DBehaviour : ElympicsMonoBehaviour, IUpdatable
 	{
 		_timerForFiring.Value -= Time.deltaTime;
 	}
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //ZDERZENIE Z KROWĄ/KARABELĄ
+
+        if (this.gameObject.name == "Player2")
+        {
+
+            if (collision.gameObject.name == "BallGreen(Clone)")
+            {
+
+                //Destroy(collision.collider.gameObject);
+                this.hp--;
+                Debug.Log(this.gameObject.name + " hp: " + hp);
+                ElympicsDestroy(collision.collider.gameObject);
+
+            }
+
+        }
+        else if (this.gameObject.name == "Player1")
+        {
+
+            if (collision.gameObject.name == "BallPurple(Clone)")
+            {
+
+                //Destroy(collision.collider.gameObject);
+                this.hp--;
+                Debug.Log(this.gameObject.name + " hp: " + hp);
+                ElympicsDestroy(collision.collider.gameObject);
+                
+            }
+
+        }
+
+
+    }
+
 }
