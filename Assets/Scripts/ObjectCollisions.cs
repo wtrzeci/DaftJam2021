8cce99717f2def1bdd745d588798754aa0b6c6b5
wtrzeci <w.trzeci@gmail.com>
2021-09-11 02:41:02 +0200

    Dodanie Destroy na kulkach, mechanika skoku dziala.

---
+++ Assets/Scripts/ObjectCollisions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Elympics;
+
+public class ObjectCollisions : ElympicsMonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ElympicsDestroy(this.gameObject);
+        Debug.Log("kolizja");
+    }
+}
